2096af2663b72a82163e6d012f528eefb5310ec1
lauro-silva44 <allvinhos@gmail.com>
2021-09-28 23:23:44 -0100

    added the text and the small logo

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -29,7 +30,19 @@ class _LoginState extends State<Login> {
               left: 0,
               right: 0,
               child: Image.asset(AppImages.person, width: 208, height: 373),
-            )
+            ),
+            Positioned(
+                bottom: size.height * 0.15,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImages.logomini),
+                    Text("Organize seus boletos em um só lugar",
+                        style: TextStyles.titleHome)
+                  ],
+                ))
           ]),
         ));
   }
